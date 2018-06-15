e18ae949f1d9ab44615012c410fcb342059abe3b
Frederick Cheung <frederick.cheung@gmail.com>
2018-06-15 08:18:21 +0100

    make specs happy again

--- spec/lib/opsgenie/heartbeat_spec.rb
+++ spec/lib/opsgenie/heartbeat_spec.rb
@@ -5,8 +5,8 @@ require 'logger'
 describe Opsgenie::Heartbeat do
   describe 'self.pulse' do
     it 'pings heartbeat request' do
-      stub = stub_request(:post, "https://api.opsgenie.com/v2/heartbeats/dressipi/ping")
-      .with(body:{ name: "dressipi"}, headers: {'Authorization': "GenieKey 123456", "Content-Type": "application/json"})
+      stub = stub_request(:get, "https://api.opsgenie.com/v2/heartbeats/dressipi/ping")
+      .with(headers: {'Authorization': "GenieKey 123456"})
       .and_return(status: 200, body: "", headers:{})
 
       Opsgenie::Heartbeat.pulse('dressipi')
@@ -25,7 +25,7 @@ describe Opsgenie::Heartbeat do
 
     it 'calls create when response is not success' do
       stub = stub_request(:get, "https://api.opsgenie.com/v2/heartbeats/dressipi")
-      .and_return(status: 500)
+      .and_return(status: 404)
       expect(Opsgenie::Heartbeat).to receive(:create).with(name:'dressipi', description:'test', interval:10, unit:'minutes', enabled: true, team: nil)
       Opsgenie::Heartbeat.ensure(name:'dressipi', interval:10, unit:'minutes', description:'test', enabled: true)
       expect(stub).to have_been_requested
@@ -45,7 +45,7 @@ describe Opsgenie::Heartbeat do
 
   describe 'self.update' do
     it 'updates the heartbeat with specified parameters' do
-      stub = stub_request(:patch, "https://api.opsgenie.com/v2/heartbeats")
+      stub = stub_request(:patch, "https://api.opsgenie.com/v2/heartbeats/dressipi")
       .with(body:{ name:'dressipi', ownerTeam: {name: 'someteam'}}, headers: {'Authorization': "GenieKey #{Opsgenie::Heartbeat.configuration.api_key}", "Content-Type": "application/json"})
       .and_return(status: 200, body: "", headers:{})
 
@@ -107,7 +107,7 @@ describe Opsgenie::Heartbeat do
       }
       it 'prints message in log file' do
         expect(logger).to receive(:info).with("Exception raised during heartbeat: #{e.message} #{e.backtrace}")
-        Opsgenie::Heartbeat.resolve_exception(e)
+        Opsgenie::Heartbeat.send(:resolve_exception,e)
       end
     end
   end
